bb82db03730f7d56a655bc7e48f0e9ec564442a3
evandavies11 <evandavies11@gmail.com>
2019-10-18 22:56:03 -0500

    Updates

---
+++ precache-manifest.63f5391fe7f6a9778ad0461a047cc494.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e641bfaced34b7f280985f6d599a407",
+    "url": "/CLICKY-GAME/index.html"
+  },
+  {
+    "revision": "b2d45427d044cd7b2bda",
+    "url": "/CLICKY-GAME/static/css/main.c7be404d.chunk.css"
+  },
+  {
+    "revision": "488cdf043428c608417c",
+    "url": "/CLICKY-GAME/static/js/2.edd039f4.chunk.js"
+  },
+  {
+    "revision": "b2d45427d044cd7b2bda",
+    "url": "/CLICKY-GAME/static/js/main.7fee25d7.chunk.js"
+  },
+  {
+    "revision": "0cde5346af6018869a20",
+    "url": "/CLICKY-GAME/static/js/runtime-main.d542360f.js"
+  },
+  {
+    "revision": "621cf67a07b404634cf35d0cea2ce41c",
+    "url": "/CLICKY-GAME/static/media/sunrise-1756274_1920.621cf67a.jpg"
+  }
+]);
